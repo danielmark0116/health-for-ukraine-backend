9af5679d275d1d0e4e258d0a0d61a280b09844c9
Daniel Grychtoł <grychtol.daniel@gmail.com>
2022-03-09 23:12:59 +0100

    Add redis and update env types

--- server/types/env.d.ts
+++ server/types/env.d.ts
@@ -2,25 +2,28 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       // POSTGRES
-      POSTGRES_PORT: string;
-      POSTGRES_USER: string;
-      POSTGRES_PASSWORD: string;
-      POSTGRES_DB: string;
+      POSTGRES_PORT: string
+      POSTGRES_USER: string
+      POSTGRES_PASSWORD: string
+      POSTGRES_DB: string
+
+      // REDIS
+      REDIS_PASSWORD: string
 
       // SERVER
-      SERVER_PORT: string;
-      MODE: string;
-      JWT_SECRET: string;
+      SERVER_PORT: string
+      MODE: string
+      JWT_SECRET: string
 
       // GOOGLE
-      GOOGLE_CLIENT_ID: string;
-      GOOGLE_CLIENT_SECRET: string;
-      GOOGLE_CALLBACK_URL: string;
+      GOOGLE_CLIENT_ID: string
+      GOOGLE_CLIENT_SECRET: string
+      GOOGLE_CALLBACK_URL: string
 
       // SENDGRID
-      SENDGRID_API_KEY: string;
+      SENDGRID_API_KEY: string
     }
   }
 }
 
-export {};
+export {}
